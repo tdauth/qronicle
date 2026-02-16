d617b19a352d646f9c0d5b49130351dd79b80086
Tamino Dauth <taminojdauth@gmail.com>
2026-02-16 08:01:04 +0100

    local files

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 cd ./build
 ./chronicle \
     --clear \
-    --amsn ../personal/amsn
-    --skype "/media/tdauth/Taminos_5.5_TB/Alte Daten/Verläufe und Lesezeichen/Kopete-Sicherung (KDE 4 Lenovo Thinkpad)/Skype (.Skype)/" \
+    --amsn ../personal/amsn \
+    --skype ../personal/skype2 \
     --kopete ../personal/kopete \
     --trillian ../personal/trillian \
     --facebook ../personal/facebook \
